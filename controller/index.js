d44024a7bea5f9fd6b6f510c28b478fe43062179
王大力 <cookiy8511@gmail.com>
2013-04-09 18:18:43 +0800

    change route rule
    change route rule

--- controller/index.js
+++ controller/index.js
@@ -9,3 +9,15 @@ exports.index = function(req, res){
 exports.list = function(req, res){
     res.render('list', { title: 'list' });
 };
+exports.today = function(req, res){
+    res.render('today', { title: 'today' });
+};
+exports.discuss = function(req, res){
+    res.render('discuss', { title: 'dis' });
+};
+exports.user = function(req, res){
+    res.render('user', { title: 'user' });
+};
+exports.nofind = function(req, res){
+    res.render('404', { title: '404' });
+};
--- route.js
+++ route.js
@@ -3,4 +3,8 @@ exports = module.exports = function(app) {
     app.get('/', site.index);
     app.get('/index', site.index);
     app.get('/list', site.list);
+    app.get('/today', site.today);
+    app.get('/discuss', site.discuss);
+    app.get('/user', site.user);
+    app.get('*', site.nofind);
 }
